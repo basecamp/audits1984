e0bff00aea3df7d7fbdfbcbd5032ec4cbd3446ab
Jorge Manrubia <jorge.manrubia@gmail.com>
2021-08-10 08:16:50 +0200

    Initial rubocop-it-all

--- app/mailers/audits1984/application_mailer.rb
+++ app/mailers/audits1984/application_mailer.rb
@@ -1,6 +1,6 @@
 module Audits1984
   class ApplicationMailer < ActionMailer::Base
-    default from: 'from@example.com'
-    layout 'mailer'
+    default from: "from@example.com"
+    layout "mailer"
   end
 end
