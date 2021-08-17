1dd47b2069d1e6b76c9f06f991c99cd898c6e154
Jorge Manrubia <jorge.manrubia@gmail.com>
2021-08-17 15:51:31 +0200

    Remove dups when multiple sensitive sessions or reviews

--- app/models/audits1984/session/auditable.rb
+++ app/models/audits1984/session/auditable.rb
@@ -4,8 +4,8 @@ module Audits1984::Session::Auditable
   included do
     has_many :audits, dependent: :destroy, class_name: "Audits1984::Audit"
 
-    scope :sensitive, -> { joins(:sensitive_accesses) }
-    scope :reviewed, -> { joins(:audits) }
+    scope :sensitive, -> { joins(:sensitive_accesses).distinct }
+    scope :reviewed, -> { joins(:audits).distinct }
     scope :approved, -> { reviewed.where("audits.status": :approved) }
     scope :flagged, -> { reviewed.where("audits.status": :flagged) }
     scope :pending, -> { where.not(id: reviewed) }
