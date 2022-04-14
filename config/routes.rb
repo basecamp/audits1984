fa26b263d8a72f9c20245b2e163088c6be5c9362
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-14 15:46:32 +0800

    Delete config directory

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Audits1984::Engine.routes.draw do
-  resources :sessions, only: %i[ index show ] do
-    resources :audits, only: %i[ create update ]
-  end
-
-  resource :filtered_sessions, only: %i[ update ]
-
-  root to: "sessions#index"
-end
